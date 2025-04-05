e8bd507f98018c3ee38bc861c0703c9abcd82cd2
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2025-04-05 14:43:02 +0300

    use .env variables

--- src/main/java/com/lowcost/db/LowCostDBConnection.java
+++ src/main/java/com/lowcost/db/LowCostDBConnection.java
@@ -1,18 +1,20 @@
 package com.lowcost.db;
 
 import lombok.SneakyThrows;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.*;
 
 public class LowCostDBConnection {
     private static Connection conn;
+    private static final Dotenv dotenv = Dotenv.configure().load();
 
     @SneakyThrows({ClassNotFoundException.class, SQLException.class})
     public static Connection getConnection() {
         if(conn == null) {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
-                    "postgres", "password");
+            conn = DriverManager.getConnection(dotenv.get("DB_URL"),
+                    dotenv.get("DB_USERNAME"), dotenv.get("DB_PASSWORD"));
         }
         return conn;
     }
